f983835006a4a7d57112edbd112b163ab59f7cd2
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-13 18:16:52 -0300

    iniciando o porjeto só

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header.jsx";
+
+
+const App = () => {
+  
+  return (
+
+    <div>
+      <Header />   
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h3>
+        Controle Financeiro
+      </h3>
+    </div>
+  );
+};
+
+export default Header;
