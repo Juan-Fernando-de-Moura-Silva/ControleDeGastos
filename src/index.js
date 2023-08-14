8866938f8037e209c984d53480eb359b191f93e4
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-14 15:45:32 -0300

    pronto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import './index.css';
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
